1b7999076b500ec7877bf7cfb6cc1355e28e9afc
rwaldron <outsideinprofits@gmail.com>
2023-09-06 21:00:09 +1000

    refactor: :rotating_light: Fixed Warning - Must contain non-null value when exiting constructor

--- RoomBook.Core/Models/Domain/RoomBookingRequest.cs
+++ RoomBook.Core/Models/Domain/RoomBookingRequest.cs
@@ -2,9 +2,9 @@ namespace RoomBook.Core.Models.Domain
 {
   public class RoomBookingRequest
   {
-    public string FirstName { get; set;}
-    public string LastName { get; set;}
-    public DateTime Date { get; set;}
-    public string Email { get; set;}
+    public string FirstName { get; set;} = string.Empty!;
+    public string LastName { get; set;} = string.Empty!;
+    public DateTime Date { get; set;} 
+    public string Email { get; set;} = string.Empty!;
   }
 }
